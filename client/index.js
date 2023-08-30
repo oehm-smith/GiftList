c18d92d4bd7625146fd29446eba921e14bfda37a
Brooke Smith <brooke@oehmsmith.com>
2023-08-30 14:46:10 +1000

    GiftList done
    * But I'm confused as the server.js says "hardcode root here".  Does that mean it shouldn't have the MerkleTree?  But it needs it to do `getProof()`.

--- client/index.js
+++ client/index.js
@@ -1,17 +1,42 @@
+// Brooke's version of this
+// Each time this is run it generates 2 names.  1 is in the niceList and the other isn't.
+// I chose to do it this way rather than have a CLI input of a name, to expedite things.
+
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const getSomeNames = () => {
+  const indexInNameList = () => Math.floor(Math.random() * niceList.length);
+
+  const nameInList = niceList[indexInNameList()];
+    // assume it will never be same as First Part Index
+  const nameNotInListFirstPart = niceList[indexInNameList()].split(' ')[0];
+  const nameNotInListSecondPart = niceList[indexInNameList()].split(' ')[0];
+  const nameNotInList = `${nameNotInListFirstPart} ${nameNotInListSecondPart}`
+  // console.log(`nameInList: ${nameInList}, nameNotInList: ${nameNotInList}`)
+  return {nameInList, nameNotInList}
+}
+
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const { nameInList, nameNotInList } = getSomeNames();
+  const yesNameInListGift = await checkForGift(nameInList)
+  const noNameNotInListGift = await checkForGift(nameNotInList)
+  console.log(`"${nameInList}, ${yesNameInListGift}"`)
+  console.log(`"${nameNotInList}, ${noNameNotInListGift}"`)
+}
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
-
-  console.log({ gift });
+checkForGift = async (name) => {
+  try {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      name
+    });
+    return gift;
+  } catch (e) {
+    console.log(`Exception: ${e.response.status} - "${e.response.data}"`)
+  }
 }
 
 main();
--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require("../utils/MerkleTree")
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
+const merkleTree = new MerkleTree(niceList);
+
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
+const root = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const {name} = req.body;
+  if (! name) {
+    return res.status(404).send(`'name' body param required`)
+  }
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  // TODO: prove that a name is in the list
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  const isInTheList = verifyProof(proof, name, root)
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
